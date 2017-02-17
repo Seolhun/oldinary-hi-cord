8d90045a25b28aadcd23bb4cf54d0a969dc34167
HunSeol <HunSeol@218.38.137.27>
2017-02-18 00:32:53 +0900

    local atmosphere build

--- hi-cord/src/main/java/com/brain/home/config/security/SecurityAppInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.brain.home.config.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityAppInitializer extends AbstractSecurityWebApplicationInitializer {
-	
-}
